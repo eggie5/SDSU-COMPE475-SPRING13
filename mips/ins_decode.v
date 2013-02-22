a451fa6f378365832a896bd1e0ce4b305953c8c3
Alex Egg <eggie5@gmail.com>
2013-02-21 19:06:33 -0800

    using instruction decoder

--- mips/ins_decode.v
+++ mips/ins_decode.v
@@ -1,5 +1,5 @@
 module Instr_Decode(
-	Instruction, Opcode, R1, R2, R3, Immediate, Jump
+	Instruction, Opcode, R1, R2, R3, Immediate, Jump, Funct
     );
 
 input [31:0] Instruction;
@@ -9,6 +9,7 @@ output [4:0] R2;
 output [4:0] R3;
 output [15:0] Immediate;
 output [25:0] Jump;
+output [5:0] Funct;
 
 assign Opcode = Instruction [31:26];
 assign R1 = Instruction[25:21];
@@ -16,6 +17,7 @@ assign R2 = Instruction[20:16];
 assign R3 = Instruction[15:11];
 assign Immediate = Instruction[15:0];
 assign Jump = Instruction[25:0];
+assign Funct = Instruction[5:0];
 
 
 
--- mips/processor.v
+++ mips/processor.v
@@ -35,22 +35,41 @@ wire ALU_SrcB;
 //lw or sd mux output
 wire [31:0] result;
 
+//writeReg mux -- this chooses [20:16] or [15:11] of instruction based on RegDst from controll
+wire [4:0] WriteReg;
+
 //sign-extender outputs
 wire [31:0] SignImm;
+
+//instruction decoder output
+reg [5:0] Opcode;
+reg [4:0] R1;
+reg [4:0] R2;
+reg [4:0] R3;
+reg [15:0] Immediate;
+reg [25:0] Jump;
+reg [5:0] Funct;
+
 
 
 ProgramCounter pc (clk, pc_in, pc_out);
 InstructionSet #(addWidth, dataWidth) instruction_set (pc_out, di, ins); //input: pc, output: instruction
-Controller mock_controller (ins[31:26], ins[5:0], MemToReg, MemWrite, Branch, ALUControl, ALUSrc, RegDest, RegWrite);//takes op and function from instruction
-RegisterFile register_file (clk, ins[25:21], ins[20:16], ins[15:11], WD3?, RD1, RD2); //FIX: the A3 input bit width is not constant
 
-SignExtender extenderbender (ins[15:0], SignImm);
-MUX21 alu_src_B_mux (RD1, SignImm, ALUSrc, ALU_SrcB); //decides what input ALU port B gets
+//decode instrution for convience
+Instr_Decode decoder (ins, Opcode, R1, R2, R3, Immediate, Jump, Funct);
+
+Controller mock_controller (Opcode, Funct, MemToReg, MemWrite, Branch, ALUControl, ALUSrc, RegDest, RegWrite);//takes op and function from instruction
+
+MUX21 who_goes_to_A3_of_reg (R2, R3, RegDest, WriteReg);
+RegisterFile register_file (clk, R1, R2, WriteReg, RegWrite, RD1, RD2); //A3 comes from mux on above line! RegWrite from controller
+
+SignExtender extenderbender (Immediate, SignImm);
+MUX21 alu_src_B_mux (RD2, SignImm, ALUSrc, ALU_SrcB); //decides what input ALU port B gets
 ALU myalu(ALUControl, RD1, ALU_SrcB, alu_out);
 
-DataMemory #(addWidth, dataWidth) dmem (clk, we, en, alu_out, RD2, dmem_out);
+DataMemory #(addWidth, dataWidth) dmem (clk, MemWrite, en, alu_out, RD2, dmem_out); //MemWrite from controller
 
-MUX21 alu_result_mux (alu_out, RD2, MemToReg, result); //0 for lw or sd inst. or 1 for R type ins. like ADD, etc
+MUX21 alu_result_mux (alu_out, dmem_out, MemToReg, result); //0 for lw or sd inst. or 1 for R type ins. like ADD, etc
 
 initial begin
 
