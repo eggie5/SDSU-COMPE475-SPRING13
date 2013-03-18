96c2bd425c669ae0935b2cfe6efbe014e06a098e
Alex Egg <eggie5@gmail.com>
2013-03-17 20:32:25 -0700

    cleanup

--- mips/reg.v
+++ mips/reg.v
@@ -18,7 +18,7 @@
 
 
 module RegisterFile //this is a ramRF memory module
-#(parameter addWidth=32, dataWidth=32)
+#(parameter addWidth=5, dataWidth=32)
 (input clk, 
 input we3, //from controller
 input [4:0] A1,
@@ -28,18 +28,14 @@ input [31:0] WD3,
 output  [31:0] RD1,
 output  [31:0] RD2);
 
-reg [dataWidth-1:0] registers [addWidth-1:0];
+reg [dataWidth-1:0] registers [2**addWidth-1:0];
 
 initial $readmemb("reg.mem", registers);
 
 
-
-	// asynchronous reads???
-/*	always @(posedge clk) begin*/
 		//these the 2 instructions operands, i.e. A & B in C=A+B
 		assign RD1 = registers[A1]; //A
 		assign RD2 = registers[A2]; //B
-/*	end*/
 
 	// synchronous writes; handles $zero register
 	always @(posedge clk) begin
