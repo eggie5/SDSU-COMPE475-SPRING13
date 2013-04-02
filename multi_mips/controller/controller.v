e1f1da286e644da153b4a99ba31e3a8024bca4e4
Alex Egg <eggie5@gmail.com>
2013-04-02 15:58:54 -0700

    almost all datapath wired up

--- multi_mips/controller/controller.v
+++ multi_mips/controller/controller.v
@@ -1,6 +1,7 @@
 module Controller
 (input clk,
 input [5:0] Opcode, Funct,
+input zero,
 output MemToReg, RegDst, IorD, PCSrc, ALUSrcA, ALUSrcB, IRWrite, MemWrite, PCWrite, Branch, RegWrite, ALUControl
 );
 
--- multi_mips/datapath/datapath.v
+++ multi_mips/datapath/datapath.v
@@ -1,6 +1,6 @@
 module datapath(
 input clk, 
-input MemToReg, RegDst, IorD, PCSrc, PCEn, ALUSrcA, [1:0] ALUSrcB, IRWrite, MemWrite, PCWrite, Branch, RegWrite,
+input MemToReg, RegDst, IorD, PCSrc, ALUSrcA, [1:0] ALUSrcB, IRWrite, MemWrite, PCWrite, Branch, RegWrite,
 input [2:0] ALUControl,
 output [5:0] Opcode, Funct //send to controller
 );
@@ -10,33 +10,69 @@ parameter addWidth = 6, dataWidth=32;
 //pc
 reg pc_clr;
 wire [PCW-1:0] pc_out;
+wire PCEn;
+wire [addWidth-1:0] pc_reg_out;
+wire [addWidth-1:0] Adr;
 
 //memory
 reg [dataWidth-1:0] mem_out;
 reg [dataWidth-1:0] instruction;
+reg [dataWidth-1:0] Data;
 
 
 //decode instrution
 wire [5:0] Opcode;
-wire [4:0] R1;
-wire [4:0] R2;
-wire [4:0] R3;
+wire [4:0] A1;
+wire [4:0] A2;
+wire [4:0] A3;
 wire [15:0] Immediate;
 wire [25:0] Jump;
 wire [5:0] Funct;
 
+//reg
+wire [dataWidth-1:0] RD1;
+wire [dataWidth-1:0] RD2;
+wire [dataWidth-1:0] A;
+wire [dataWidth-1:0] B;
+wire [dataWidth-1:0] WD3_mux_out;
+wire [dataWidth-1:0] a3_mux_out;
+
+//alu
+wire [dataWidth-1:0] srcA_mux_out;
+wire [dataWidth-1:0] srcB_mux_out;
+wire [dataWidth-1:0] ALUResult;
+wire [dataWidth-1:0] ALUOut; // after reg
+wire [dataWidth-1:0] alu_mux_out;
+wire alu_zero;
 
 
-PC #(addWidth) pc(clk, pc_clr, PCEn, pc_out);
-Memory #(addWidth, dataWidth) mem (clk, MemWrite?, pc_out, mem_out);
+
+//PC #(addWidth) pc(clk, pc_clr, PCEn, pc_out); // there really is no more individual PC module -- it just dff
+DFF #(addWidth) pc(clk, PCEn, alu_mux_out, pc_reg_out);
+MUX21 #(addWidth) pc_mux(pc_reg_out, ALUOut, IorD, Adr);
+
+Memory #(addWidth, dataWidth) mem (clk, MemWrite?, pc_out, B, mem_out);
 DFF #(dataWidth) dff1 (clk, IRWrite, mem_out, instruction);
+Instr_Decode decoder (instruction, Opcode, A1, A2, A3, Immediate, Jump, Funct);
+DFF #(dataWidth) dff2 (clk, 1, mem_out, Data); //this is reg after mem, leads to reg file
 
-Instr_Decode decoder (instruction, Opcode, R1, R2, R3, Immediate, Jump, Funct);
-Register reg(clk, RegWrite, R1, R2, R3,....muxes...);
+MUX21 #(dataWidth) a3_mux(A2, A3, RegDst, a3_mux_out);
+MUX21 #(dataWidth) wd3_mux(?, Data, MemToReg, WD3_mux_out);
+Register reg(clk, RegWrite, A1, A2, a3_mux_out, WD3_mux_out, RD1, RD2);
+DFF #(dataWidth) dff_a (clk, 1, RD1, A);
+DFF #(dataWidth) dff_b (clk, 1, RD2, B);
+
+MUX21 #(dataWidth) srcA_mux(pc_out, A, ALUSrcA, srcA_mux_out); // waht is the width on this one? 
+MUX41 #(dataWidth) srcB_mux(B, 1, SignImm, SignImm<<2, ALUSrcB, srcB_mux_out) ;
+ALU alu(ALUControl, srcA_mux_out, srcB_mux_out, ALUResult, alu_zero);
+DFF #(dataWidth) dff_alu_result(clk, 1, ALUResult, ALUOut);
+MUX21 #(dataWidth) alu_mux(ALUResult, ALUOut, PCSrc, alu_mux_out);
+
+assign PCEn = ((alu_zero & Branch) & PCWrite); // goes to pc
 
 
 
-ALU alu();
+
 SignExtend sext();
 
 endmodule
