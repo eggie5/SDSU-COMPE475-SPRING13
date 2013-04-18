0f6962e721314bfaa2e68459c4befe4b273a92cc
Alex Egg <eggie5@gmail.com>
2013-04-17 21:01:31 -0700

    updates for datapath

--- pipelined/datapath/datapath.v
+++ pipelined/datapath/datapath.v
@@ -46,7 +46,9 @@ wire [dataWidth-1:0] RD1;
 wire [dataWidth-1:0] RD2;
 wire [dataWidth-1:0] SrcAE;
 wire [dataWidth-1:0] B;
-wire [dataWidth-1:0] WD3_mux_out;
+wire [dataWidth-1:0] ResultW;
+wire [addData-1:0] WriteRegM;
+wire [addData-1:0] WriteRegW;
 
 //alu
 wire [dataWidth-1:0] ALUOutM; // after reg
@@ -67,33 +69,41 @@ Memory #(addWidth, dataWidth) mem (clk, 0, pc_out, 1'bx, mem_out);
 //fetch reg
 DFF #(dataWidth) decode_reg (clk, 0, 1, mem_out, InstrD);
 
+//DECODE REGION 
 //Decoder
 Decoder decoder (InstrD, Opcode, A1, A2, A3, Immediate, Jump, Funct);
 
 //Register File
-Register #(addWidth-1, dataWidth) reg_file (clk, RegWriteW, A1, A2, a3_mux_out, WD3_mux_out, RD1, RD2);
+Register #(addWidth-1, dataWidth) reg_file (clk, RegWriteW, A1, A2, WriteRegW, ResultW, RD1, RD2);
 DFF #(dataWidth) execute_reg_rd1 (clk, 0, 1, RD1, SrcAE);
 DFF #(dataWidth) execute_reg_rd2 (clk, 0, 1, RD2, B);
 DFF #(dataWidth) execute_reg_rte (clk, 0, 1, A2, RtE);
 DFF #(dataWidth) execute_reg_rde (clk, 0, 1, A3, RdE);
-MUX21 #(addWidth-1) a3_mux (RtE, RdE, RegDstE, WriteRegE);
 
 //SEX
 SignExtender sext(Immediate, SignImm);
-DFF #(dataWidth) decode_dff_sex (clk, 0, 1, SignImm, SignImmE);
+DFF #(dataWidth) execute_reg_sex (clk, 0, 1, SignImm, SignImmE);
 
+
+//EXECUTE REGION
 //ALU
+MUX21 #(addWidth-1) a3_mux (RtE, RdE, RegDstE, WriteRegE);
 MUX21 #(dataWidth) srcB_mux (B, SignImmE, ALUSrcE, SrcBE) ; 
 ALU alu (ALUControlE, SrcAE, SrcBE, ALUResult, alu_zero);
+
+//MEMWRITE REGION
 DFF #(dataWidth) mem_reg_zero (clk, 0, 1, alu_zero, ZeroM);
 DFF #(dataWidth) mem_reg_alu (clk, 0, 1, ALUResult, ALUOutM);
 DFF #(dataWidth) mem_reg_write_data (clk, 0, 1, B, WriteDataM);
 DFF #(dataWidth) mem_reg_write_reg (clk, 0, 1, WriteRegE, WriteRegM);
- 
+
 //memory
 Memory #(addWidth, dataWidth) mem (clk, MemWriteM, ALUOutM, WriteDataM, mem_out);
+
+//WRITEBACK REGION
 DFF #(dataWidth) write_reg_memout (clk, 0, 1, mem_out, ReadDataW);
 DFF #(dataWidth) write_reg_aluout (clk, 0, 1, ALUOutM, ALUOutW);
+DFF #(dataWidth) write_reg_writeregW (clk, 0, 1, WriteRegM, WriteRegW);
 
 MUX21 #(dataWidth) mem_out_mux (ALUOutW, ReadDataW, MemToRegW, ResultW) ; 
 
